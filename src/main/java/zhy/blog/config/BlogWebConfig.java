e79f303203407d5f490e9d1da6f18d9a300bce68
zhy <996544315@qq.com>
2019-07-04 13:32:16 +0800

    springboot配置mvc的新方式

--- src/main/java/zhy/blog/config/BlogWebConfig.java
+++ src/main/java/zhy/blog/config/BlogWebConfig.java
@@ -2,14 +2,13 @@ package zhy.blog.config;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import zhy.blog.web.UrlInterceptor;
 
 @Component
-public class BlogWebConfig extends WebMvcConfigurerAdapter {
+public class BlogWebConfig implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new UrlInterceptor());
-		super.addInterceptors(registry);
 	}
 }
